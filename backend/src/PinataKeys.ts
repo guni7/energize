adfaff0e349b8a011694f0c7091fae45d5239a59
guni7 <57868372+guni7@users.noreply.github.com>
2022-08-04 21:17:02 +0530

    @feature ipfs pin backend

---
+++ backend/src/PinataKeys.ts
@@ -0,0 +1,4 @@
+export default {
+  apiKey: "1cb57f71a4f0c6213e55",
+  apiSecret: "6bceda556367754766bddd9b8334aca687208a78196b8216c943c0351ce0dcb1"
+};
---
+++ backend/src/index.ts
@@ -0,0 +1,108 @@
+import express from "express";
+import pinataSDK from "@pinata/sdk";
+import fs from "fs";
+const cors = require("cors");
+const multer = require("multer");
+
+const app = express();
+const upload = multer({ dest: "uploads/" });
+const port = process.env.NODE_ENV === "production" ? process.env.PORT : 8080; // default port to listen
+let pinata: any;
+if (process.env.NODE_ENV === "production") {
+    pinata = pinataSDK(process.env.PINATA_API_KEY, process.env.PINATA_SECRET_KEY);
+} else {
+    const PinataKeys = require("./PinataKeys").default;
+    pinata = pinataSDK(PinataKeys.apiKey, PinataKeys.apiSecret);
+}
+const corsOptions = {
+    origin: ["http://localhost:3000", "https://my-cool-nft-app.com"],
+    optionsSuccessStatus: 200
+};
+app.use(cors(corsOptions));
+app.use(express.json({ limit: "50mb" }));
+app.use(
+    express.urlencoded({ limit: "50mb", extended: true, parameterLimit: 50000 })
+);
+app.get("/", (req, res) => {
+    res.send("Hello developers!");
+});
+
+app.post("/mint", upload.single("image"), async (req, res) => {
+    console.log('minting');
+    const multerReq = req as any;
+    if (!multerReq.file) {
+        console.log('no file')
+        res.status(500).json({ status: false, msg: "no file provided" });
+    } else {
+        const fileName = multerReq.file.filename;
+        // tests Pinata authentication
+        let test = await pinata
+            .testAuthentication()
+            .catch((err: any) => {
+                res.status(500).json(JSON.stringify(err)) 
+                console.log('no authh')
+            });
+        console.log(test);
+        // creates readable stream
+        const readableStreamForFile = fs.createReadStream(`./uploads/${fileName}`);
+        const options: any = {
+            pinataMetadata: {
+                name: req.body.title.replace(/\s/g, "-"),
+                keyvalues: {
+                    description: req.body.description
+                }
+            }
+        };
+        const pinnedFile = await pinata.pinFileToIPFS(
+            readableStreamForFile,
+            options
+        );
+        console.log(pinnedFile);
+        if (pinnedFile.IpfsHash && pinnedFile.PinSize > 0) {
+            // remove file from server
+            fs.unlinkSync(`./uploads/${fileName}`);
+            // pins metadata
+            const metadata = {
+                name: req.body.title,
+                description: req.body.description,
+                symbol: "ENER",
+                artifactUri: `ipfs://${pinnedFile.IpfsHash}`,
+                displayUri: `ipfs://${pinnedFile.IpfsHash}`,
+                creators: [req.body.creator],
+                decimals: 0,
+                thumbnailUri: "https://tezostaquito.io/img/favicon.png",
+                is_transferable: true,
+                shouldPreferSymbol: false
+            };
+
+            const pinnedMetadata = await pinata.pinJSONToIPFS(metadata, {
+                pinataMetadata: {
+                    name: "ENER-metadata"
+                }
+            });
+
+            if (pinnedMetadata.IpfsHash && pinnedMetadata.PinSize > 0) {
+                res.status(200).json({
+                    status: true,
+                    msg: {
+                        imageHash: pinnedFile.IpfsHash,
+                        metadataHash: pinnedMetadata.IpfsHash
+                    }
+                });
+            console.log(pinnedFile.IpfsHash)
+            } else {
+                console.log('p');
+                res
+                    .status(500)
+                    .json({ status: false, msg: "metadata were not pinned" });
+            }
+        } else {
+            console.log("not piined")
+            res.status(500).json({ status: false, msg: "file was not pinned" });
+        }
+    }
+});
+
+app.listen(port, () => {
+    console.log(`server started at http://localhost:${port}`);
+});
