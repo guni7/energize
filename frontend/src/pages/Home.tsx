17ce9ce500b63052b1f6b86eec5e6924452f3c20
guni7 <57868372+guni7@users.noreply.github.com>
2022-08-12 11:35:27 +0530

    heroku deploy

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,12 +1,26 @@
 import { useSelector } from "react-redux"
 import Navbar from "../components/Navbar"
+import { selectBeaconConnection } from "../features/tezos/selectors";
 import Mint from "./Mint"
 
 export const Home = () => {
+    const beaconConnection = useSelector(selectBeaconConnection);
 
     return (
-        <div className="bg-gray-900 h-screen">
-            <Mint />
+        <div className="bg-gray-800 h-screen">
+            {beaconConnection ?
+                <Mint />
+                :
+                <div className="flex flex-col">
+                    <Navbar />
+                    <div className="text-4xl text-pink-400 self-center justify-self-center mt-56">
+                        connect wallet to 
+                    </div>``
+                    <div className="text-6xl text-pink-400 self-center justify-self-center">
+                        energize
+                    </div>
+                </div>
+            }
         </div>
     )
 }
