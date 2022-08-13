3f32ef0cb842101b2df4e6a66a53a5f3d1dce464
guni7 <57868372+guni7@users.noreply.github.com>
2022-08-13 16:19:31 +0530

    cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,6 @@
 import pinataSDK from "@pinata/sdk";
 const fs = require("fs");
-//const cors = require("cors");
+const cors = require("cors");
 const multer = require("multer");
 const express = require("express");
 
@@ -15,28 +15,13 @@ if (process.env.NODE_ENV === "production") {
     const PinataKeys = require("./PinataKeys").default;
     pinata = pinataSDK(PinataKeys.apiKey, PinataKeys.apiSecret);
 }
-//const corsOptions = {
-//    origin: ["*", "https://capable-begonia-127106.netlify.app/"],
-//    optionsSuccessStatus: 200,
-//    methods: ['GET', 'PUT', 'POST', 'OPTIONS'],
-//};
-//app.use(cors(corsOptions));
+const corsOptions = {
+    origin: "https://capable-begonia-127106.netlify.app/",
+    optionsSuccessStatus: 200,
+    methods: ['GET', 'PUT', 'POST', 'OPTIONS'],
+};
+app.use(cors(corsOptions));
 
-app.use((req: any, res: any, next: any) => {
-    res.header("Access-Control-Allow-Origin", "*")
-    res.header(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested, Content-Type, Accept Authorization"
-    )
-    if (req.method === "OPTIONS") {
-        res.header(
-            "Access-Control-Allow-Methods",
-            "POST, PUT, PATCH, GET, DELETE", "OPTIONS"
-        )
-        return res.status(200).json({})
-    }
-    next()
-})
 
 app.use(express.json({ limit: "50mb" }));
 app.use(
