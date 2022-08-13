00066d30290d0717c7548a2b8c80d279a1d8676e
guni7 <57868372+guni7@users.noreply.github.com>
2022-08-13 13:53:57 +0530

    cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -22,21 +22,12 @@ if (process.env.NODE_ENV === "production") {
 //};
 //app.use(cors(corsOptions));
 
-app.use((req: any, res:any, next: any) => {
-    res.header("Access-Control-Allow-Origin", "*")
-    res.header(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested, Content-Type, Accept Authorization"
-    )
-    if (req.method === "OPTIONS") {
-        res.header(
-            "Access-Control-Allow-Methods",
-            "POST, PUT, PATCH, GET, DELETE"
-        )
-        return res.status(200).json({})
-    }
-    next()
-})
+app.use(function(req: any, res: any, next: any) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    next();
+  });
 
 app.use(express.json({ limit: "50mb" }));
 app.use(
